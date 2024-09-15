f73a71dca2157ffebacbc1f5e37406520a381a68
Hqnnqh <hannah@diefluchs.at>
2024-09-15 11:07:54 +0200

    Refactor host connection

--- src/host/main.c
+++ src/host/main.c
@@ -10,28 +10,23 @@
 #define FREQUENCY_MS 50000
 #define PORT 8888
 
-struct Sockets {
-  int server_socket;
-  int client_socket;
-};
-
-struct Sockets create_connection(int port) {
-  struct Sockets sockets;
+void create_connection(int *server_socket, int *client_socket, int port) {
   struct sockaddr_in server, client;
 
   // create socket
-  sockets.server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockets.server_socket == -1) {
+  *server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (*server_socket == -1) {
     perror("Could not create socket.");
     exit(EXIT_FAILURE);
   }
 
   // Set SO_REUSEADDR to allow immediate reuse of the socket after it is closed
-   int opt = 1;
-   if (setsockopt(sockets.server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-     perror("Could not set SO_REUSEADDR.");
-     exit(EXIT_FAILURE);
-   }
+  int opt = 1;
+  if (setsockopt(*server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) <
+      0) {
+    perror("Could not set SO_REUSEADDR.");
+    exit(EXIT_FAILURE);
+  }
 
   // initialize server
   server.sin_family = AF_INET;
@@ -39,34 +34,31 @@ struct Sockets create_connection(int port) {
   server.sin_port = htons(PORT);
 
   // bind server socket
-  if (bind(sockets.server_socket, (struct sockaddr *)&server, sizeof(server)) <
-      0) {
+  if (bind(*server_socket, (struct sockaddr *)&server, sizeof(server)) < 0) {
     perror("Could not bind server socket.");
     exit(EXIT_FAILURE);
   }
 
   // listen for incoming connections; backlog 3 indicates 3 clients can connect
   // simultaniously in queue.
-  listen(sockets.server_socket, 3);
+  listen(*server_socket, 3);
 
   // accept incoming connection
   int connection_size = sizeof(struct sockaddr_in);
-  sockets.client_socket =
-      accept(sockets.server_socket, (struct sockaddr *)&client,
-             (socklen_t *)&connection_size);
+  *client_socket = accept(*server_socket, (struct sockaddr *)&client,
+                          (socklen_t *)&connection_size);
 
-  if (sockets.client_socket < 0) {
+  if (*client_socket < 0) {
     perror("Server could not accept client connection.");
     exit(EXIT_FAILURE);
   }
-
-  return sockets;
 }
 
 int main(int argc, char *argv[]) {
 
+  int server_socket, client_socket;
   // create tcp connection
-  struct Sockets sockets = create_connection(PORT);
+  create_connection(&server_socket, &client_socket, PORT);
 
   // connect to X server
   Display *display = XOpenDisplay(NULL);
@@ -84,12 +76,13 @@ int main(int argc, char *argv[]) {
 
   // todo: capture screen, keyboard input
 
-  while(1) {}
+  while (1) {
+  }
 
   // clean up
   XCloseDisplay(display);
-  close(sockets.client_socket);
-  close(sockets.server_socket);
+  close(client_socket);
+  close(server_socket);
 
   return 0;
 }
